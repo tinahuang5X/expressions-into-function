c235048feeb3d2b0ba9ed223501e5a605af90371
Tina Huang <tingyuhuang@aol.com>
2017-07-12 06:28:23 -0700

    cleaned up codes

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,8 @@
-// Clean up the code below with a single function called `personalIntroduction`
-// The new code should have the exact same functionality and output.
+function personalIntroduction(name, height, favoriteColor) {
+  return "My name is " + name + ", and my height is " + height + ", and my favorite color is " + favoriteColor;
+}
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+console.log(personalIntroduction("Bob", "72", "red"));
+console.log(personalIntroduction("Samantha", "65", "orange"));
+console.log(personalIntroduction("Christopher", "69", "green"));
+console.log(personalIntroduction("Tamera", "63", "blue"));
